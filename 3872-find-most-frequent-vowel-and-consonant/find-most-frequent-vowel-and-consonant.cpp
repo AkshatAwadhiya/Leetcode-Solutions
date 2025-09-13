db5f1d1a661b4851fe9a212ff6aba9d207cc3723
AkshatAwadhiya <akshatawadhiya@gmail.com>
2025-09-13 22:51:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (90.16%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -7,16 +7,16 @@ public:
             return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
         };
 
-        int maxV = 0, maxC = 0;
+        int maxX = 0, maxY = 0;
         for (char v : string("aeiou")) {
-            maxV = max(maxV, freq[v - 'a']);
+            maxX = max(maxX, freq[v - 'a']);
         }
         for (int i = 0; i < 26; i++) {
             char ch = 'a' + i;
             if (isVowel(ch)) continue;
-            maxC = max(maxC, freq[i]);
+            maxY = max(maxY, freq[i]);
         }
 
-        return maxV + maxC;
+        return maxX + maxY;
     }
 };
